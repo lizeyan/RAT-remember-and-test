84d08f9e6471a83299a7ed8e2ecd79afe7936a98
lizeyan <zy-li14@hotmail.com>
2015-06-10 14:47:54 +0800

    UML类图

--- recite.h
+++ recite.h
@@ -22,68 +22,11 @@ public:
     void DoReview(Word* m, std::ostream& osout, std::istream& input, int huihe);
     void ReciteControl(std::ostream& osout, std::istream& input);
     bool GetDone(){return done;}
-    int ReciteNeedTime(Word* m){//返回一个单词需要连续背会几次才能够算是会了
-        int lin = 0;
-        time_t t = time(0);
-        char tmp[5];
-        strftime( tmp, sizeof(tmp), "%j",localtime(&t) );
-        int today = atoi(tmp);
-        lin = today - m->reviewDay;
-        if(lin>10){
-            return 1;
-        }else if(lin>4){
-            return 2;
-        }else{
-            return 3;
-        }
-    }
-    int ReciteThisTime(){//返回以现有还剩的所需背诵的单词数量为参量，得到一组需要背几个单词
-        return reciteWord.size()/10+1;
-    }
-    int ReviewThisTime(){
-        if(ReciteThisTime()>4){
-            return 20-ReciteThisTime();
-        }else{
-            return 16-ReciteThisTime();
-        }
-    }
-    std::vector<int> SearchReview(std::vector<Word*> &m, int num){//选择需要复习的单词，按照权重
-        std::vector<int> choose;
-        std::vector<int> lin;
-        lin.clear();
-        choose.clear();
-        for(int i=0; i<m.size(); i++){
-            choose.push_back(m[i]->quanReview);
-        }
-        for(int i=0; i<(m.size()>num? num : m.size()); i++){
-            int all=0;
-            for(int k=0; k<choose.size(); k++){
-                all+=choose[k];
-            }
-            if(all==0) all=1;
-            int output=rand()%all+1;
-            int number=0;
-            int j;
-            for(j=0; j<choose.size(); j++){
-                number+=choose[j];
-                if(number>=output){
-                    break;
-                }
-            }
-            lin.push_back(j);
-            choose[j]=0;
-        }
-        return lin;
-    }
-    void ReviewQuanUpdate(Word* m){
-        m->quanReview=10*m->wrong/(m->wrong+m->right+1) > 0? 10*m->wrong/(m->wrong+m->right+1) : 1;
-        if(m->zu){
-            m->quanReview-=10;
-        }else{
-            m->quanReview+=10;
-        }
-        m->quanReview=m->quanReview>0?m->quanReview:1;
-    }
+    int ReciteNeedTime(Word* m);
+    int ReciteThisTime();
+    int ReviewThisTime();
+    std::vector<int> SearchReview(std::vector<Word*> &m, int num);
+    void ReviewQuanUpdate(Word* m);
 };
 
 #endif /* defined(_______recite__) */
